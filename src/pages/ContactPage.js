ccea58b77fa30675e761bdf2a6aa8916aaaab0f7
mahamed81 <mahamed10ahmed@gmail.com>
2022-08-25 09:04:40 -0500

    contact page

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ContactPage = () => {
   return (
     <div>
-      <h1>This is Contact page</h1>
+      <h1>This is going to be the contact page</h1>
     </div>
   );
 }
